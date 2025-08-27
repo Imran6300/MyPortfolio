c0e2ed2c2cc783b7377fb8a7ebadf340d0df554e
Syed Mubashir Ahmed <148939929+Imran6300@users.noreply.github.com>
2025-08-27 12:59:39 +0530

    Update Image.jsx

--- src/Components/HeroContent/Image.jsx
+++ src/Components/HeroContent/Image.jsx
@@ -5,7 +5,7 @@ const Image = () => {
   return (
     <div className={Css.HeroImage}>
       <FadeContent blur duration={1000} easing="ease-out" initialOpacity={0}>
-        <img src={`http://localhost:5173/MyPortfolio/Imran.png`} alt="Imran" />
+        <img src={`./Imran.png`} alt="Imran" />
       </FadeContent>
     </div>
   );
